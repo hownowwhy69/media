abc5ed721a306e23a5adc670f670bd7ac394cce7
hownowwhy69 <rajwaderotts69@gmail.com>
2026-09-18 02:04:13 +0530

    Set default values for environment variables in config.py
    Updated environment variable defaults for API_ID, API_HASH, BOT_TOKEN, OWNER_IDS, MONGO_DB, and CHANNEL_IDS.

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_IDS = list(map(int, getenv("OWNER_IDS", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-CHANNEL_IDS = list(map(int, getenv("CHANNEL_IDS", "").split()))
+API_ID = int(getenv("API_ID", "32853799"))
+API_HASH = getenv("API_HASH", "7618973ca6fa27183f6df27dfba88f26")
+BOT_TOKEN = getenv("BOT_TOKEN", "8803183382:AAFPiHVHEYMZKaa8n6rhhs3RQbGgY9GJ8HM")
+OWNER_IDS = list(map(int, getenv("OWNER_IDS", "738363992").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://tanmaysingh87op_db_user:RQKGjrYs5U
+NvHvtm@cluster0.7gpqqce.mongodb.net/?appName=Cluster0")
+CHANNEL_IDS = list(map(int, getenv("CHANNEL_IDS", "-1004442937928").split()))
 
